7d8a503a40926ada39531cc85ecce75d9a2201aa
Jacob Vorreuter <jacob.vorreuter@gmail.com>
2013-10-06 00:08:48 -0700

    load images

--- README.md
+++ README.md
@@ -3,5 +3,8 @@
 ```
 export AWS_ACCESS_KEY_ID=
 export AWS_SECRET_ACCESS_KEY=
-export S3_IMAGE_UPLOADER_BUCKET_NAME=
+export S3_BUCKET_NAME=
+export S3_IMAGE_PREFIX=
+export AUTH_USER=
+export AUTH_PASSWORD=
 ```
--- server.js
+++ server.js
@@ -4,8 +4,11 @@ var WebSocketServer = require('ws').Server
   , app = express()
   , port = process.env.PORT || 5000
   , s3_bucket_name = process.env.S3_BUCKET_NAME
-  , s3_image_prefix = process.env.S3_IMAGE_PREFIX;
+  , s3_image_prefix = process.env.S3_IMAGE_PREFIX
+  , auth_user = process.env.AUTH_USER
+  , auth_password = process.env.AUTH_PASSWORD;
 
+app.use(express.basicAuth(auth_user, auth_password));
 app.use(express.static(__dirname + '/'));
 
 var server = http.createServer(app);
@@ -17,14 +20,17 @@ var AWS = require('aws-sdk');
 var s3 = new AWS.S3();
 
 
-function stream_images(ws) {
-  s3.listObjects({Bucket: s3_bucket_name, Prefix: s3_image_prefix, MaxKeys: 10}, function(err, data) {
+function stream_images(marker, ws) {
+  s3.listObjects({Bucket: s3_bucket_name, Prefix: s3_image_prefix, MaxKeys: 3, Marker: marker}, function(err, data) {
     if(err != null) console.log("s3.listObjects [err]: " + err);
-    for (var index in data.Contents) {
-      s3.getObject({Bucket: s3_bucket_name, Key: data.Contents[index].Key, ResponseContentType: "image/jpeg"}, function(err, data) {
-        if(err != null) console.log("s3.getObject [err]: " + err);
-        ws.send(data.Body.toString('base64'), function() {});
-      });
+    if(data.Contents.length > 0) {
+      var new_marker = data.Contents[data.Contents.length-1].Key
+      for (var index in data.Contents) {
+        s3.getObject({Bucket: s3_bucket_name, Key: data.Contents[index].Key, ResponseContentType: "image/jpeg"}, function(err, data) {
+          if(err != null) console.log("s3.getObject [err]: " + err);
+          ws.send(JSON.stringify({Marker: new_marker, Modified: data.LastModified, Body: data.Body.toString('base64')}), function() {});
+        });
+      }
     }
   });
 }
@@ -33,7 +39,9 @@ var wss = new WebSocketServer({server: server});
 console.log('websocket server created');
 wss.on('connection', function(ws) {
     console.log('websocket connection open');
-    stream_images(ws);
+    ws.on('message', function(marker) {
+        stream_images(marker, ws);
+    });
     ws.on('close', function() {
         console.log('websocket connection close');
     });
