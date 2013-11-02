109ec5d3a8f7773f897c3fb5afcf90d8fc53aa63
Jacob Vorreuter <jacob.vorreuter@gmail.com>
2013-11-01 17:04:45 -0700

    refactoring image streaming to preserve order. also restyling

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var WebSocketServer = require('ws').Server
   , http = require('http')
   , express = require('express')
   , app = express()
+  , moment = require('moment')
   , port = process.env.PORT || 5000
   , s3_bucket_name = process.env.S3_BUCKET_NAME
   , s3_image_prefix = process.env.S3_IMAGE_PREFIX
@@ -24,16 +25,25 @@ var s3 = new AWS.S3();
 
 function stream_images(marker, ws) {
   // fetch a batch of object names/meta data from s3
-  s3.listObjects({Bucket: s3_bucket_name, Prefix: s3_image_prefix, MaxKeys: s3_batch_size, Marker: marker}, function(err, data) {
+  s3.listObjects({Bucket: s3_bucket_name, Prefix: s3_image_prefix, MaxKeys: s3_batch_size, Marker: marker}, function(err, list_data) {
     if(err != null) { console.log("s3.listObjects [err]: " + err); return; }
-    if(data.Contents.length > 0) {
-      var new_marker = data.Contents[data.Contents.length-1].Key
-      for (var index in data.Contents) {
-        s3.getObject({Bucket: s3_bucket_name, Key: data.Contents[index].Key, ResponseContentType: "image/jpeg"}, function(err, data) {
-          if(err != null) console.log("s3.getObject [err]: " + err);
-          ws.send(JSON.stringify({Marker: new_marker, Modified: data.LastModified, Body: data.Body.toString('base64')}), function() {});
-        });
-      }
+    if(list_data.Contents.length > 0) {
+      var new_marker = list_data.Contents[list_data.Contents.length-1].Key;
+      var payload = new Array();
+      collect_and_stream_images(ws, list_data, 0, payload, new_marker);
+    }
+  });
+}
+
+function collect_and_stream_images(ws, list_data, index, payload, new_marker) {
+  s3.getObject({Bucket: s3_bucket_name, Key: list_data.Contents[index].Key, ResponseContentType: "image/jpeg"}, function(err, obj_data) {
+    if(err != null) console.log("s3.getObject [err]: " + err);
+    payload.push({Modified: moment(obj_data.LastModified).format("dddd, MMMM Do YYYY, h:mm:ss a"), Body: obj_data.Body.toString('base64')});
+    if (index == list_data.Contents.length-1) {
+      console.log("sending " + payload.length + " images");
+      ws.send(JSON.stringify({Marker: new_marker, Images: payload}), function() {});
+    } else {
+      collect_and_stream_images(ws, list_data, index+1, payload, new_marker);
     }
   });
 }
@@ -42,12 +52,15 @@ function stream_images(marker, ws) {
 var wss = new WebSocketServer({server: server});
 console.log('websocket server created');
 wss.on('connection', function(ws) {
-    console.log('websocket connection open');
-    ws.on('message', function(marker) {
-        // fetch a batch of images from s3 and stream the content to the client
-        stream_images(marker, ws);
-    });
-    ws.on('close', function() {
-        console.log('websocket connection close');
-    });
+  console.log('websocket connection open');
+  ws.on('message', function(marker) {
+    // fetch a batch of images from s3 and stream the content to the client
+    stream_images(marker, ws);
+  });
+  ws.on('close', function() {
+    console.log('websocket connection close');
+  });
+  setInterval(function(){
+    ws.send(JSON.stringify({Heartbeat: 1}));
+  }, 30 * 1000);
 });
